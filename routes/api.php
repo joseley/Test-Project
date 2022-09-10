45b54d12c49051bb70d6c7ec40b94f7545196c0f
Jose Ley <josemanuel.leyrodriguez@gmail.com>
2022-09-09 19:19:07 -0500

    [JL] Register checkout route

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/check-in', [ShopperQueueController::class, 'checkIn']);
+Route::post('/check-out', [ShopperQueueController::class, 'checkOut']);

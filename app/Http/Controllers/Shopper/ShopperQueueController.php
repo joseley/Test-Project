967d72f2bf1c7276452077fafaeab5ec4a7c3c4f
Jose Ley <josemanuel.leyrodriguez@gmail.com>
2022-09-09 19:25:10 -0500

    [JL] Create check out feature (API)

--- app/Http/Controllers/Shopper/ShopperQueueController.php
+++ app/Http/Controllers/Shopper/ShopperQueueController.php
@@ -16,16 +16,16 @@ class ShopperQueueController extends Controller
             'first_name' => 'required',
             'last_name' => 'required',
             'email' => 'required|email',
-            'location_id' => 'required|integer'
+            'location_id' => 'required|integer|exists:locations,id'
         ]);
 
-        $location = Location::findOrFail($validatedData['location_id']);
+        $location = Location::find($validatedData['location_id']);
         
         // Statuses must be configured in env variables, this can be comming from DB, but exists
         // the posibility the names or initial status change
         $statuses = new \stdClass();
-        $statuses->active = env('GROCERY_STORE_STATUS_ACTIVE_ID ', 1);
-        $statuses->pending = env('GROCERY_STORE_STATUS_PENDING_ID ', 3);
+        $statuses->active = config('services.shopperqueue.status.active', 1);
+        $statuses->pending = config('services.shopperqueue.status.pending', 3);
         
         $activeShoppers = Shopper::where('status_id', $statuses->active)
             ->where('location_id', $location->id)
@@ -55,7 +55,40 @@ class ShopperQueueController extends Controller
             } else {
                 $response['message'] = 'Something went wrong, please try again';
             }
-            
+
+            return response()->json($response, 500);
+        }
+    }
+
+    public function checkOut(Request $request) {
+
+        $validatedData = $request->validate([
+            'shopper_id' => 'required|exists:shoppers,id',
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $shopper = Shopper::find($validatedData['shopper_id']);
+        $shopper->user_id = $validatedData['user_id'];
+        $shopper->status_id = config('services.shopperqueue.status.completed', 2);
+        $shopper->check_out = now();
+
+        try {
+            $shopper->save();
+
+            return response()->json([
+                'shopper' => $shopper
+            ], 200);
+        }catch(\Exception $e) {
+            $response['message'] = env('APP_DEBUG', true) ? $e->getMessage() : 'Something went wrong, please try again';
+            if (env('APP_DEBUG', true)) {
+                $response['message'] = $e->getMessage();
+                $response['payload'] = [
+                    'shopper' => $shopper
+                ];
+            } else {
+                $response['message'] = 'Something went wrong, please try again';
+            }
+
             return response()->json($response, 500);
         }
     }

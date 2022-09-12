503d6326c6ff7db1b890a355725aa78af12cf4a8
Jose Ley <josemanuel.leyrodriguez@gmail.com>
2022-09-12 16:10:01 -0500

    [JL] Scheduled task for refresing all locations queues

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\Shopper\ShopperQueueController;
+use App\Models\Store\Location\Location;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +26,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function() {
+            $locations = Location::all();
+
+            foreach($locations as $location) {
+                $shopperController = new ShopperQueueController();
+                $shopperController->refreshQueue($location);
+            }
+        })->everyMinute();
     }
 
     /**
